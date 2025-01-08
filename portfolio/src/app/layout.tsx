40b82b463ece223f0b8ca2f4474bdd9ff3c97b95
austin-area61 <austinonyango.area61@gmail.com>
2025-01-08 10:26:47 +0300

    added a use effect for mouse            position

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,23 +1,44 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Austin Onyango | Software Engineer",
-  description:
-    "Personal portfolio showcasing my work and experience as a software engineer",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#0a192f] text-gray-300`}>
+        {children}
+        <div
+          className="hover-effect"
+          style={
+            {
+              "--mouse-x": `${mousePosition.x}px`,
+              "--mouse-y": `${mousePosition.y}px`,
+            } as React.CSSProperties
+          }
+        />
+      </body>
     </html>
   );
 }
